a5a54c44288597024f8dc56ab763129f611b5c73
danceinthedark <2215629628@qq.com>
2021-07-24 18:56:31 +0800

    users模块完成

--- HinghwaDict/HinghwaDict/urls.py
+++ HinghwaDict/HinghwaDict/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('home/',TemplateView.as_view(template_name="home.html"),name='home')
+    path('users/', include('user.urls', namespace='users')),
+    path('login/', include('user.urls', namespace='login')),
+    path('users', views.register),
+    path('login', views.login),
+    path('website/', include('website.urls', namespace='website'))
 ]
