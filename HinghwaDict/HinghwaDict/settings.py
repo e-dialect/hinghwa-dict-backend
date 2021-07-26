552f193fbdaab48188f6d98cd7aa40dc7ed91fb1
FrogDar <1270610465@qq.com>
2021-07-27 02:00:54 +0800

    fix: static

--- HinghwaDict/HinghwaDict/settings.py
+++ HinghwaDict/HinghwaDict/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILESDIRS = [
+	os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
