4a0ef581651ba06c2c232dfc97f612995796557a
FrogDar <1270610465@qq.com>
2021-07-27 01:07:11 +0800

    build: dockerize

--- HinghwaDict/HinghwaDict/settings.py
+++ HinghwaDict/HinghwaDict/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
