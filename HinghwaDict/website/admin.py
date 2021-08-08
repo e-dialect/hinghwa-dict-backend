0a573ffc1b8063b540debf7159db54ea3fbada84
danceinthedark <2215629628@qq.com>
2021-08-08 21:38:15 +0800

    website模块get返回
    由返回id变成返回具体内容

--- HinghwaDict/website/admin.py
+++ HinghwaDict/website/admin.py
@@ -4,7 +4,7 @@ from .models import Website
 
 
 class WebsiteAdmin(admin.ModelAdmin):
-    list_display = ['announcements', 'hot_articles', 'word_of_the_day', 'carousal']
+    list_display = ['announcements', 'hot_articles', 'word_of_the_day', 'carousel']
 
 
 admin.site.register(Website, WebsiteAdmin)
