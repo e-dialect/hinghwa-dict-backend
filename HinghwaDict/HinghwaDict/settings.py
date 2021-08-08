994b2382c573de9c0bb8d62d852f038ec7cea787
FrogDar <1270610465@qq.com>
2021-08-08 13:48:28 +0800

    admin路由&&部署微调

--- HinghwaDict/HinghwaDict/settings.py
+++ HinghwaDict/HinghwaDict/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/login'
