51b76cf403e8bee050b5c6d33942e7c26a815db1
danceinthedark <2215629628@qq.com>
2021-09-08 17:15:08 +0800

    bug 音频mpeg文件不能在手机播放
    将audio区文件全部缓存为.mp3

--- HinghwaDict/website/views.py
+++ HinghwaDict/website/views.py
@@ -281,6 +281,8 @@ def files(request):
             if request.method == "POST":
                 file = request.FILES.get("file")
                 type, suffix = str(file.content_type).split('/')
+                if type == 'audio':
+                    suffix = 'mp3'
                 time = timezone.now().__format__("%Y_%m_%d")
                 filename = time + '_' + random_str(15) + '.' + suffix
                 type_folder = os.path.join(settings.MEDIA_ROOT, type)
