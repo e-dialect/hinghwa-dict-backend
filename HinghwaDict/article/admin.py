bf4abea4d0cee5b93555af7c58852827c81b9615
danceinthedark <2215629628@qq.com>
2021-10-08 17:39:34 +0800

    article增加visibility字段及其功能&admin批量处理
    1、article的搜索和批获取只返回visibility=True字段
    2、仔细查询若为visibility=True或查询者为作者或者管理员，则返回文章，否则404
    3、admin的批处理在动作选项处，能够批量审核

--- HinghwaDict/article/admin.py
+++ HinghwaDict/article/admin.py
@@ -1,15 +1,43 @@
 from django.contrib import admin
-
+from django.contrib import messages
+from django.utils.translation import ngettext
 from .models import Article, Comment
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author', 'title', 'publish_time', 'update_time', 'views', 'like']
+    list_display = ['id', 'author', 'title', 'publish_time', 'update_time', 'views', 'like', 'visibility']
     search_fields = ['author__username', 'title', 'description']
     list_filter = ['author', 'publish_time']
     date_hierarchy = 'publish_time'
     ordering = ('id', 'author__id', '-publish_time', '-views', '-update_time')
+    list_editable = ['visibility', ]
     list_per_page = 50
+
+    def pass_visibility(self, request, queryset):
+        for article in queryset:
+            article.visibility = True
+            article.save()
+        updated = len(queryset)
+        self.message_user(request, ngettext(
+            '%d 篇文章被成功标记为可见。',
+            '%d 篇文章被成功标记为可见。',
+            updated,
+        ) % updated, messages.SUCCESS)
+    pass_visibility.short_description = "所选 文章 通过审核"
+
+    def withdraw_visibility(self, request, queryset):
+        for article in queryset:
+            article.visibility = False
+            article.save()
+        updated = len(queryset)
+        self.message_user(request, ngettext(
+            '%d 篇文章被成功标记为不可见。',
+            '%d 篇文章被成功标记为不可见。',
+            updated,
+        ) % updated, messages.SUCCESS)
+    withdraw_visibility.short_description = "所选 文章 不通过审核"
+
+    actions = ['pass_visibility', 'withdraw_visibility']
 
 
 class CommentAdmin(admin.ModelAdmin):
