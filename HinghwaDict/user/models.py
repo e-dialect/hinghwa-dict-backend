76c6570a7a9828640395a6454ab475c42fc67a7a
danceinthedark <2215629628@qq.com>
2022-01-19 23:49:40 +0800

    user model defaultavatar
    修改userInfo avatar字段的默认头像链接

--- HinghwaDict/user/models.py
+++ HinghwaDict/user/models.py
@@ -8,8 +8,9 @@ class UserInfo(models.Model):
     nickname = models.CharField(blank=True, max_length=100, verbose_name='昵称')
     birthday = models.DateField(blank=True, default='1970-1-1', verbose_name='生日')
     telephone = models.CharField(blank=True, max_length=50, verbose_name='电话')
-    avatar = models.URLField(default='http://118.25.147.215:8080/image/defaultUser.png',
-                             blank=True, verbose_name='头像')  # TODO defaultURL得改
+    avatar = models.URLField(default='https://hinghwadict-1259415432.cos.ap-shanghai.myqcloud.com/'
+                                     'files/image/1/2022/01/19/bMuzwU97XtBZHve.jpg',
+                             blank=True, verbose_name='头像')
     county = models.CharField(blank=True, max_length=100, verbose_name="县区")
     town = models.CharField(blank=True, max_length=100, verbose_name="乡镇")
 
