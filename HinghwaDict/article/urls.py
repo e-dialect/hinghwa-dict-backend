9a9a99612c73076f95d5211f55ee85c14f068f7c
danceinthedark <57979622+danceinthedark@users.noreply.github.com>
2022-03-20 09:49:15 +0800

    build article visibility
    增加文章审核接口

--- HinghwaDict/article/urls.py
+++ HinghwaDict/article/urls.py
@@ -7,6 +7,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', searchArticle),
     path('/<int:id>', manageArticle),
+    path('/<int:id>/visibility', manageArticleVisibility),
     path('/<int:id>/like', like),
     path('/<int:id>/comments', comment),
     path('/comments', searchComment)
