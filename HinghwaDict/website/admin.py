a81a45cd7e2f8e13f62f4f5c5d88c71d2a1fb23d
danceinthedark <57979622+danceinthedark@users.noreply.github.com>
2022-04-01 00:26:23 +0800

    enhance notification admin
    中文标签

--- HinghwaDict/website/admin.py
+++ HinghwaDict/website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django_apscheduler.apps import DjangoApschedulerConfig
+from django_apscheduler.models import DjangoJob, DjangoJobExecution
 from notifications.admin import NotificationAdmin
-
+from notifications.models import Notification
 from .models import Website, DailyExpression
 
 
@@ -21,9 +22,12 @@ admin.site.register(DailyExpression, DailyExpressionAdmin)
 
 DjangoApschedulerConfig.verbose_name = '定时任务'
 DjangoApschedulerConfig.verbose_name_plural = '定时任务'
+DjangoJobExecution._meta.verbose_name_plural = '任务执行情况'
+DjangoJobExecution._meta.verbose_name = '任务执行情况'
+DjangoJob._meta.verbose_name_plural = '任务'
+DjangoJob._meta.verbose_name = '任务'
 # 好像没用
-# Config.verbose_name = '站内通信'
-# Config.verbose_name_plural = '站内通信'
+Notification._meta.verbose_name_plural = '站内通知'
 NotificationAdmin.search_fields = ['recipient__username', 'actor__username']
 NotificationAdmin.list_display = list_display = ('id', 'recipient', 'actor', 'level',
                                                  'verb', 'timestamp', 'unread', 'public')
