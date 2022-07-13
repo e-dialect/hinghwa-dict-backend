80ae5aeeff987b9d4164c995dd373271bfe6a7ec
lll123github <157721376@qq.com>
2022-07-13 21:44:47 +0800

    style: remake the style of music_normal.py
    add some spaces around "->"

--- hinghwa-dict-backend/music/dto/music_normal.py
+++ hinghwa-dict-backend/music/dto/music_normal.py
@@ -1,14 +1,15 @@
 from ..models import Music
 
-def music_normal(music: Music)->dict:
-    response={
+
+def music_normal(music: Music) -> dict:
+    response = {
         "id": music.id,
         "source": music.source,
         "title": music.title,
         "artist": music.artist,
         "cover": music.cover,
         "likes": music.like(),
-        "contributor": music.contributor.id, 
+        "contributor": music.contributor.id,
         "visibility": music.visibility
     }
     return response
