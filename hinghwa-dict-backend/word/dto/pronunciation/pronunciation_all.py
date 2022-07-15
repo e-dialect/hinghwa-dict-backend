6c513640e2871829572f6f9108f07365f7eb37a4
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2022-07-15 20:53:31 +0800

    refactor： create dto
    create dto
    pronunciation_normal
    pronunciation_all
    pronunciation_simple already exists,and this model is not used

---
+++ hinghwa-dict-backend/word/dto/pronunciation/pronunciation_all.py
@@ -0,0 +1,28 @@
+from word.models import Pronunciation
+from user.dto.user_all import user_all
+
+
+# 返回语音的所有信息
+def pronunciation_all(pronunciation: Pronunciation) -> dict:
+    user = pronunciation.contributor
+    response = {
+        "id": pronunciation.id,
+        "word_id": pronunciation.word.id,
+        "word_word": pronunciation.word.word,
+        "source": pronunciation.source,
+        "ipa": pronunciation.ipa,
+        "pinyin": pronunciation.pinyin,
+        "contributor": user_all(user),
+        "county": pronunciation.county,
+        "town": pronunciation.town,
+        "visibility": pronunciation.visibility,
+        "verifier": {
+            "nickname": pronunciation.verifier.user_info.nickname,
+            "avatar": pronunciation.verifier.user_info.avatar,
+            "id": pronunciation.verifier.id,
+        }
+        if pronunciation.verifier
+        else None,
+        "granted": pronunciation.granted(),
+    }
+    return response
