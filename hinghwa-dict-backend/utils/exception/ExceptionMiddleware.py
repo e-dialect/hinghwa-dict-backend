6e15982341fb9f01bb005f49773d0c67ab329dc7
sheeplin <1270610465@qq.com>
2022-07-25 00:28:30 +0800

    fix: fix ExceptionMiddleware

--- hinghwa-dict-backend/utils/exception/ExceptionMiddleware.py
+++ hinghwa-dict-backend/utils/exception/ExceptionMiddleware.py
@@ -3,16 +3,18 @@ from django.middleware.common import MiddlewareMixin
 
 from .types.common import CommonException
 
+
 class ExceptionMiddleware(MiddlewareMixin):
     """统一异常处理中间件"""
 
-    def process_exception(self, request, exception)->JsonResponse:
+    def process_exception(self, request, exception) -> JsonResponse:
         """
         统一异常处理
         :param request: 请求对象
         :param exception: 异常对象
         :return:
         """
-        if isinstance(exception, Exception):
-            return CommonException(exception).response()
-        return exception.response()
+        print(type(exception))
+        if isinstance(exception, CommonException):
+            return exception.response()
+        return CommonException(exception).response()
