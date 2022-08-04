b8fdc4bdae692b65cd220992a9fc1f4748533191
lll123github <157721376@qq.com>
2022-08-05 01:29:36 +0800

    fix: import user.password_validation
    instead of password_validation

--- hinghwa-dict-backend/user/views.py
+++ hinghwa-dict-backend/user/views.py
@@ -13,7 +13,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from notifications.models import Notification
 from user.dto.user_all import user_all
-from password_validation import password_validator
+from user.password_validation import password_validator
 
 from website.views import (
     random_str,
