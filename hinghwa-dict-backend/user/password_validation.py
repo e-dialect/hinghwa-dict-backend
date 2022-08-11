7d2e694eab892b7bee14ce177d7035eab15c5f7f
lll123github <157721376@qq.com>
2022-08-12 00:07:45 +0800

    fix: fix the condition of validating a password
    fix the condition of validating a password

--- hinghwa-dict-backend/user/password_validation.py
+++ hinghwa-dict-backend/user/password_validation.py
@@ -25,5 +25,5 @@ def password_validator(password):
     """
     检验（新）密码是否符合规范
     """
-    if len(password) < 6 and len(password) > 32:
+    if len(password) < 6 or len(password) > 32:
         raise InvalidPassword
