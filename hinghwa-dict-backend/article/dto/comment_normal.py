b1f909d14798d92abd7dcdbe56fadc71eeb94e3d
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2022-11-08 21:29:48 +0800

    feat : add message to AT0404 (#187)
    * feat : add message to AT0404
    
    * refactor: add the test of article module
    
    * refactor： update article.json
    
    * style: format tests
    
    Co-authored-by: sheeplin <1270610465@qq.com>

--- hinghwa-dict-backend/article/dto/comment_normal.py
+++ hinghwa-dict-backend/article/dto/comment_normal.py
@@ -1,11 +1,13 @@
 from ..models import Comment
+from user.dto.user_simple import user_simple
 
 
 # 返回评论基本信息
 def comment_normal(comment: Comment) -> dict:
+    user = comment.user
     response = {
         "id": comment.id,
-        "user": comment.user.id,
+        "user": user_simple(user),
         "content": comment.content,
         "time": comment.time.__format__("%Y-%m-%d %H:%M:%S"),
         "parent": comment.parent_id if comment.parent else 0,
