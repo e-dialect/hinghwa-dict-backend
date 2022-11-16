1758fffae49ecfe1037dbdc790b090449988377f
sheeplin <1270610465@qq.com>
2022-11-16 14:16:17 +0800

    feat: add inspect function

--- hinghwa-dict-backend/quiz/admin.py
+++ hinghwa-dict-backend/quiz/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ngettext
 from .models import Quiz
 
 
@@ -6,11 +8,49 @@ from .models import Quiz
 
 
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ["id", "question", "answer"]
-    list_filter = []
-    search_fields = ["id", "question"]
-    ordering = ["id"]
+    list_display = ["id", "question", "answer","visibility"]
+    list_filter = ["visibility"]
+    search_fields = ["id", "question", "answer"]
+    ordering = ["id", "question"]
     list_per_page = 50
+
+    def pass_visibility(self, request, queryset):
+        for item in queryset:
+            item.visibility = True
+            item.save()
+        updated = len(queryset)
+        self.message_user(
+            request,
+            ngettext(
+                "%d 个测试题被成功标记为可见。",
+                "%d 个测试题被成功标记为可见。",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+    pass_visibility.short_description = "所选 测试题 通过审核"
+
+    def withdraw_visibility(self, request, queryset):
+        for item in queryset:
+            item.visibility = False
+            item.save()
+        updated = len(queryset)
+        self.message_user(
+            request,
+            ngettext(
+                "%d 个测试题被成功标记为不可见。",
+                "%d 个测试题被成功标记为不可见。",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+    withdraw_visibility.short_description = "所选 测试题 不通过审核"
+
+    actions = ["pass_visibility", "withdraw_visibility"]
 
 
 admin.site.register(Quiz, QuizAdmin)
