85da8412b68522934d095e33466420d69bf5e24a
sheeplin <1270610465@qq.com>
2022-11-16 16:06:57 +0800

    refactor: refactor SingleApplication class

--- hinghwa-dict-backend/word/application/urls.py
+++ hinghwa-dict-backend/word/application/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path(
         "/applications", csrf_exempt(MultiApplication.as_view())
     ),  # WD0401POST WD0403GET
-    path("/applications/<int:id>", manageApplication),  # WD0402GET WD0404PUT
+    path(
+        "/applications/<int:id>", csrf_exempt(SingleApplication.as_view())
+    ),  # WD0402GET WD0404PUT
 ]
