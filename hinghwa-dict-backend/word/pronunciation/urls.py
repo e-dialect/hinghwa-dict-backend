408e3dfa5ce6b2454cedc8390ad3d00b62894920
lll123github <157721376@qq.com>
2023-01-07 12:15:17 +0800

    style: update the class name of PronunciationRanking
    update the class name of PronunciationRanking

--- hinghwa-dict-backend/word/pronunciation/urls.py
+++ hinghwa-dict-backend/word/pronunciation/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("/<int:id>/visibility", managePronunciationVisibility),
     path("/<int:id>/examine", managePronunciationVisibility),
     path("/<str:ipa>", combinePronunciation),
-    path("/ranking",csrf_exempt(pronunciationRanking).as_view()),   # PN0205语音审核
+    path("/ranking", csrf_exempt(PronunciationRanking).as_view()),  # PN0205语音审核
 ]
