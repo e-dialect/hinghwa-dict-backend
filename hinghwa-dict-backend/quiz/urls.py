f24e6ec6db3cef09911f6ca8767d3b98634d0cef
Norton_Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-01-11 15:35:02 +0800

    feat: add get quiz paper (#220)
    * feat: add get quiz paper
    
    新增 获取测试题试卷功能
    重构 word模块部分异常处理
    
    * styled: remove redundant codes
    
    * styled: remove redundant codes

--- hinghwa-dict-backend/quiz/urls.py
+++ hinghwa-dict-backend/quiz/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("/<int:id>", csrf_exempt(SingleQuiz.as_view())),  # QZ0101 QZ0103  QZ0104
     path("/<int:id>/visibility", csrf_exempt(ManageVisibility.as_view())),  # QZ0105
     path("/random", csrf_exempt(RandomQuiz.as_view())),  # QZ0202
+    path("/paper", csrf_exempt(QuizPaper.as_view())),  # QZ0203
 ]
