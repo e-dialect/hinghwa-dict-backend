2730d9dc845153148e156ffb5be1095d012d38ce
Norton_Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-01-11 21:33:02 +0800

    feat: resolve wechat binding (#222)
    * feat: solve wechat bouding problem
    
    新增 账户微信已绑定情况下的重绑定
    同时重构该接口代码
    
    * sytle : format code
    
    * refactor: refactor class UpdateWechat
    
    * style: update user.view.py
    
    * fix: fix the judge of overwrite
    
    修复 US0304接口请求字段overwrite判断问题
    调整 NotBoundWechat异常类类型
    
    * style: format code
    
    * feat: add response message
    
    新增 NotBoundWechat 响应信息，提示对应用户未绑定微信

--- hinghwa-dict-backend/user/urls.py
+++ hinghwa-dict-backend/user/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("<int:id>", manageInfo),
     path("<int:id>/pronunciation", pronunciation),
     path("<int:id>/password", csrf_exempt(UpdatePassword.as_view())),  # US0302
-    path("<int:id>/wechat", updateWechat),
+    path("<int:id>/wechat", csrf_exempt(UpdateWechat.as_view())),  # US0304 US0305
     path("<int:id>/email", updateEmail),
     path("app", app),
     path("forget", forget),
