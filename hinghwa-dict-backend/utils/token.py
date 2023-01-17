105ca6fd81ffa90262bb42a341d4b3b8e7c76c14
sheeplin <1270610465@qq.com>
2023-01-17 17:10:50 +0800

    feat: add get_user function
    close #268

--- hinghwa-dict-backend/utils/token.py
+++ hinghwa-dict-backend/utils/token.py
@@ -3,8 +3,9 @@ import string
 import numbers
 import jwt
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,AnonymousUser
 from django.utils import timezone
+from django.http import request
 from utils.exception.types.forbidden import OnlyAdminException
 from utils.exception.types.forbidden import ForbiddenException
 from utils.exception.types.unauthorized import (
@@ -83,3 +84,26 @@ def generate_token(user: User) -> string:
     # 不知道为什么，本地显示jwt.encode是Object但是服务器显示是str
     token = jwt.encode(payload, settings.JWT_KEY, algorithm="HS256")
     return token
+
+def get_user(request:request) -> User:
+    """
+    从request中判定当前用户
+    :param request:
+    :return User:
+    """
+    try:   
+            token = request.headers["token"]
+            key = settings.JWT_KEY
+            info = jwt.decode(token, key, algorithms=["HS256"])
+            # exam expiration time
+            if info["exp"] < timezone.now().timestamp():
+                return AnonymousUser()
+            # get user
+            user = User.objects.get(id=info["id"])
+            print(user.username)
+            # exam username
+            if user.username != info["username"]:
+                return AnonymousUser()
+            return user
+    except:
+        return AnonymousUser()
