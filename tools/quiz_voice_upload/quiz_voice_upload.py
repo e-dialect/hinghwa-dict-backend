e2be3a239945762d312ffd4d717e53b642556c6b
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-03-15 16:23:44 +0800

    feat: add base_url to quiz_voice_upload
    新增 base_url 于 quiz_voice_upload.py
    新增 小工具readme介绍

--- tools/quiz_voice_upload/quiz_voice_upload.py
+++ tools/quiz_voice_upload/quiz_voice_upload.py
@@ -1,44 +1,32 @@
 import json
 import os
 from os.path import splitext
-
-import requests
-
+from requests_toolbelt import sessions
 from environs import Env
 
+http = sessions.BaseUrlSession(base_url="https://api.pxm.edialect.top")
 env = Env()
 env.read_env(path=".env")
 username = env.str("user")
 password = env.str("password")
 data = {"username": username, "password": password}
-token = requests.post(
-    f"https://api.pxm.edialect.top/login", data=json.dumps(data)
-).json()["token"]
+token = http.post("/login", data=json.dumps(data)).json()["token"]
 url = input("请输入文件夹路径:")
 #   url = os.path.pardir
-file = os.listdir(url)
-for f in file:
+files = os.listdir(url)
+for f in files:
     str = splitext(f)[-1]
     if str == ".m4a" or str == ".mp3":
         id = f.split(".")[0].split("/")[-1]
-        #   正式
-        res = requests.get(
-            f"https://api.pxm.edialect.top/quizzes/{id}", headers={"token": token}
-        ).json()
-        #   测试服
-        #   res = requests.get(f"https://api.pxm.test.edialect.top/quizzes/{id}", headers={'token': token}).json()
-        #   本地
-        #   res = requests.get(f"http://127.0.0.1:8000/quizzes/{id}", headers={'token': token}).json()
+        res = http.get(f"/quizzes/{id}", headers={"token": token}).json()
         result = {"quiz": {}}
         result["quiz"]["question"] = res["quiz"]["question"]
         result["quiz"]["options"] = res["quiz"]["options"]
         result["quiz"]["answer"] = res["quiz"]["answer"]
         result["quiz"]["explanation"] = res["quiz"]["explanation"]
         result["quiz"]["voice_source"] = f"https://cos.edialect.top/quizzes/{id}{str}"
-        requests.put(
-            f"https://api.pxm.edialect.top/quizzes/{id}",
+        http.put(
+            f"/quizzes/{id}",
             data=json.dumps(result),
             headers={"token": token},
         )
-        # requests.put(f"https://api.pxm.test.edialect.top/quizzes/{id}", data=json.dumps(result), headers={'token': token})
-        # requests.put(f"http://127.0.0.1:8000/quizzes/{id}", data=json.dumps(result), headers={'token': token})
