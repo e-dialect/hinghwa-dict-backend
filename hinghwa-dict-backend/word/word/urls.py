94ba042e2ed1e7e3d0caeb253b24f99530b2fc9b
Norton_Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-04-18 16:36:05 +0800

    refactor(word): refactor word management  (#295)
    * fix: issue281
    
    修复WD0103词语修改逻辑，重构WD0101 WD0103 WD0104
    当前数据库可保证修改后无冗余记录出现
    
    * style: add note
    
    * style: format code
    
    * Update e2e.yml
    
    ---------
    
    Co-authored-by: sheeplin <1270610465@qq.com>

--- hinghwa-dict-backend/word/word/urls.py
+++ hinghwa-dict-backend/word/word/urls.py
@@ -6,7 +6,9 @@ app_name = "word.word"
 
 urlpatterns = [
     path("", searchWords),  # WD0102POST WD0201GET WD0202PUT
-    path("/<int:id>", manageWord),  # WD0101GET WD0103PUT
+    path(
+        "/<int:id>", csrf_exempt(ManageWord.as_view())
+    ),  # WD0101GET WD0103PUT WD0104DELETE
     path("/add", load_word),  # WD0301POST
     path("/upload_standard", upload_standard),  # WD0302POST
     path("/phonetic_ordering", csrf_exempt(PhoneticOrdering.as_view())),  # WD0501
