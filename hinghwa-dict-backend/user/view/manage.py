a181d796df8578d4210803b485d434a043b5a3ba
CapooL <2053217188@qq.com>
2023-11-02 00:17:51 +0800

    fix(user):delete unnecessary files

--- hinghwa-dict-backend/user/view/manage.py
+++ hinghwa-dict-backend/user/view/manage.py
@@ -59,6 +59,11 @@ class Manage(View):
                 "word_uploaded": user.contribute_words.count(),
                 "article_views": user.articles.aggregate(Sum("views"))["views__sum"]
                 or 0,
+                # TODO 去除播放量相关（需要确认前端全部删除）
+                "listened": user.contribute_pronunciation.aggregate(Sum("views")).get(
+                    "views__sum"
+                )
+                or 0,
             },
         }
 
