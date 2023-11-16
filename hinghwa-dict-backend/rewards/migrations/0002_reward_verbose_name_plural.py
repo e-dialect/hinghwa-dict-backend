4ec744ccbc23c4b59a945f7e95b9bc951832e11e
sheeplin <1270610465@qq.com>
2023-11-16 17:01:07 +0800

    refactor: black

--- hinghwa-dict-backend/rewards/migrations/0002_reward_verbose_name_plural.py
+++ hinghwa-dict-backend/rewards/migrations/0002_reward_verbose_name_plural.py
@@ -4,26 +4,25 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('rewards', '0001_initial'),
+        ("rewards", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='order',
-            options={'verbose_name': '订单', 'verbose_name_plural': '订单'},
+            name="order",
+            options={"verbose_name": "订单", "verbose_name_plural": "订单"},
         ),
         migrations.AlterModelOptions(
-            name='product',
-            options={'verbose_name': '商品', 'verbose_name_plural': '商品'},
+            name="product",
+            options={"verbose_name": "商品", "verbose_name_plural": "商品"},
         ),
         migrations.AlterModelOptions(
-            name='title',
-            options={'verbose_name': '头衔', 'verbose_name_plural': '头衔'},
+            name="title",
+            options={"verbose_name": "头衔", "verbose_name_plural": "头衔"},
         ),
         migrations.AlterModelOptions(
-            name='transaction',
-            options={'verbose_name': '积分记录', 'verbose_name_plural': '积分记录'},
+            name="transaction",
+            options={"verbose_name": "积分记录", "verbose_name_plural": "积分记录"},
         ),
     ]
