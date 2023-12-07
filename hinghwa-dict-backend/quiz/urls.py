786fba1fcc99a5d937dacdb8a5256b4d5b747040
CapooL <111524029+CapooL@users.noreply.github.com>
2023-12-07 20:45:13 +0800

    Update urls.py

--- hinghwa-dict-backend/quiz/urls.py
+++ hinghwa-dict-backend/quiz/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path("/<int:id>", csrf_exempt(SingleQuiz.as_view())),  # QZ0101 QZ0103  QZ0104
     path("/<int:id>/visibility", csrf_exempt(ManageVisibility.as_view())),  # QZ0105
     path("/random", csrf_exempt(RandomQuiz.as_view())),  # QZ0202
-
     path("/records", csrf_exempt(QuizRecordAll.as_view())),  # QZ0401 QZ0402
     path(
         "/records/<str:record_id>", csrf_exempt(QuizRecordSingle.as_view())
