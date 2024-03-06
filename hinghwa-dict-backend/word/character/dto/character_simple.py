1f3258311490b15db16834fde408c972dad4efa3
CapooL <111524029+CapooL@users.noreply.github.com>
2024-03-06 23:03:53 +0800

    feat(characters): use pinyin in keywords to look up characters (#356)
    * feat(word): add CH0205
    
    支持搜索框通过拼音查询单字
    
    * test(word): add test files
    
    * feat(character): add character_simple.py and fix regular expression
    
    * test(character): update test files

---
+++ hinghwa-dict-backend/word/character/dto/character_simple.py
@@ -0,0 +1,51 @@
+from ...models import Character, Word, Pronunciation
+
+
+def character_simple(character: Character) -> dict:
+    dic = {}
+    word = Word.objects.filter(standard_pinyin=character.pinyin).filter(
+        word=character.character
+    )
+    word_id = word[0].id if word.exists() else None
+    source = Pronunciation.objects.filter(pinyin=character.pinyin)
+    source_value = source[0].source if source.exists() else None
+
+    # 构建字典
+    key = (character.character, character.traditional)
+    if key not in dic:
+        dic[key] = []
+
+    dic[key].append(
+        {
+            "id": character.id,
+            "shengmu": character.shengmu,
+            "ipa": character.ipa,
+            "pinyin": character.pinyin,
+            "yunmu": character.yunmu,
+            "shengdiao": character.shengdiao,
+            "character": character.character,
+            "county": character.county,
+            "town": character.town,
+            "traditional": character.traditional,
+            "word": {"id": word_id} if word else None,
+            "source": {"source": source_value} if source else None,
+        }
+    )
+
+    ans = []
+    for key, value in dic.items():
+        char, traditional = key
+        result_list = [
+            {"county": item["county"], "town": item["town"], "characters": item}
+            for item in value
+        ]
+        ans.append(
+            {
+                "label": char,
+                "traditional": traditional,
+                "characters": result_list,
+            }
+        )
+    ans = ans[0]
+
+    return ans
