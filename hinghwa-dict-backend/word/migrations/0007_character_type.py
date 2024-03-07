b16d03f6710c829946edf1746f8180d849b8d8a6
CapooL <2053217188@qq.com>
2024-03-07 19:47:12 +0800

    style(character): try to reformat

--- hinghwa-dict-backend/word/migrations/0007_character_type.py
+++ hinghwa-dict-backend/word/migrations/0007_character_type.py
@@ -2,14 +2,15 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+
     dependencies = [
-        ('word', '0006_list_ship'),
+        ("word", "0006_list_ship"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='character',
-            name='type',
-            field=models.CharField(max_length=20, null=True, verbose_name='读音类型'),
+            model_name="character",
+            name="type",
+            field=models.CharField(max_length=20, null=True, verbose_name="读音类型"),
         ),
     ]
