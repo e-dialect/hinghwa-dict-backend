8a05c2c23f67a11fa6921d2cce3b13212b962427
xiaoxiangc <3187907625@qq.com>
2026-09-01 11:39:18 +0800

    Fix conflicting word migration graph

---
+++ hinghwa-dict-backend/word/migrations/0012_merge_20260901_000000.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("word", "0011_application_approved"),
+        ("word", "0011_semanticindexstate"),
+    ]
+
+    operations = []
